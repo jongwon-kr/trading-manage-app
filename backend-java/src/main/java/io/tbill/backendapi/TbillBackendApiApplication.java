f1b75c3b9bbb2782a1c6873192bc053934fd9fc5
jongwon-kr <growjong8802@gmail.com>
2025-11-03 14:54:04 +0900

    [Init] Springboot 프로젝트 초기 생성

---
+++ backend-java/src/main/java/io/tbill/backendapi/TbillBackendApiApplication.java
@@ -0,0 +1,13 @@
+package io.tbill.backendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbillBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbillBackendApiApplication.class, args);
+    }
+
+}
---
+++ backend-java/src/test/java/io/tbill/backendapi/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tbill.backendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
