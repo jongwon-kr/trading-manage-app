587303caa35e58c4c081afb3412ba74c4434f609
jongwon-kr <growjong8802@gmail.com>
2025-11-04 14:33:27 +0900

    [Init] local, prod, test 환경 설정 분리, 환경 변수 추가

--- backend-java/src/main/java/io/tbill/backendapi/TbillBackendApiApplication.java
+++ backend-java/src/main/java/io/tbill/backendapi/TbillBackendApiApplication.java
@@ -1,5 +1,6 @@
 package io.tbill.backendapi;
 
+import io.github.cdimascio.dotenv.Dotenv; // 1. import
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TbillBackendApiApplication {
 
     public static void main(String[] args) {
+
+        // 환경 변수 로드
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(TbillBackendApiApplication.class, args);
     }
-
 }
